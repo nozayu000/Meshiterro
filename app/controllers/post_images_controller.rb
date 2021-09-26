addf84174da3ecad9ee0b7c8b3edfcda13d489ac
EC2 Default User <ec2-user@ip-172-31-38-213.ec2.internal>
2021-09-26 04:56:56 +0000

    page add

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -13,12 +13,8 @@ def create
 end
 
 def index
- @post_images = PostImage.page(params[:page]).reverse_order
-  # 下記の記述では一覧画面に全ての投稿画像が表示されデータが重くなる
-  # @post_images = PostImage.all
-  # それを回避するために下記の記述に直す（kaminariがページャで利用するテンプレートを作成）
-  
-end 
+  @post_images = PostImage.page(params[:page]).reverse_order
+end
 
 def show
   @post_image = PostImage.find(params[:id])
